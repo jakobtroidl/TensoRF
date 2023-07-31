41d2e99e2cd2278584a7bd9c5777f5d7abf2cbab
jakobtroidl <jtroidl@g.harvard.edu>
2023-07-31 16:59:17 -0400

    fix: SDF values computed correctly for watertight meshes

--- dataGenerator/objects.py
+++ dataGenerator/objects.py
@@ -13,26 +13,39 @@ class Object3D:
         self.name = name
         self.res = resolution
         self.level = 2 / self.res
-        self.mesh_scale = 0.8
+        # self.mesh_scale = 0.8
         self.normalize()
 
     def __repr__(self):
         return f'Object3D(vertices={len(self.mesh.vertices)}, faces={len(self.mesh.faces)})'
 
     def normalize(self):
-        vertices = self.mesh.vertices
-        bbmin = vertices.min(0)
-        bbmax = vertices.max(0)
-        center = (bbmin + bbmax) * 0.5
-        scale = 2.0 * self.mesh_scale / (bbmax - bbmin).max()
-        self.mesh.vertices = (vertices - center) * scale
+        # Step 1: Compute the bounding box
+        min_coords = np.min(self.mesh.vertices, axis=0)
+        max_coords = np.max(self.mesh.vertices, axis=0)
+
+        # Step 2: Calculate the center of the bounding box
+        center = (min_coords + max_coords) / 2.0
+
+        # Step 3: Translate the object to the origin
+        self.mesh.vertices -= center
+
+        # Step 4: Scale the object
+        max_extent = np.max(max_coords - min_coords)
+        self.mesh.vertices /= max_extent
+
+        print("{} mesh normalized".format(self.name))
 
     def sample_sdf(self, as_tensor=True):
         # Compute the signed distance field of the mesh
         # @param size: the size of the SDF grid, result will be a size x size x size np array or tensor
         # @param as_tensor: whether to return the SDF as a numpy array or as a pytorch tensor
         # @return: the SDF grid as a pytorch tensor
-        sdf = mesh2sdf.compute(self.mesh.vertices, self.mesh.faces, self.res, fix=True, level=self.level, return_mesh=False)
+        sdf = mesh2sdf.compute(self.mesh.vertices, self.mesh.faces, self.res, fix=False, level=self.level, return_mesh=False)
+
+        print("Min SDF value: ", np.min(sdf))
+        print("Max SDF value: ", np.max(sdf))
+
         if as_tensor:
             sdf = torch.from_numpy(sdf)
         return sdf
@@ -53,7 +66,7 @@ class Object3D:
             vutils.save_image(img, path + "/" + f'{i}.png', normalize=True)
 
 
-    def store(self, sdf, path, as_tensor=True):
+    def store(self, sdf, path, as_tensor=False):
         parent = "{}/{}".format(path, self.name)
         if not os.path.exists(parent):
             # Create the directory
